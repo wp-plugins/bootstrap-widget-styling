f8a9ec952e6ab512a92478a95d8b62d5e06d8edc
ryankienstra <ryankienstra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-27 20:52:06 +0000

    Compatible up to WordPress 4.1.
    git-svn-id: https://plugins.svn.wordpress.org/bootstrap-widget-styling/tags/1.0.3@1034096 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bootstrap-widget-styling.php
+++ bootstrap-widget-styling.php
@@ -10,7 +10,7 @@ License: GPL2
 
 */
 
-if ( ! defined( 'WPINC' )  ) {
+if ( ! defined( 'WPINC' )	) {
  die ;
 }
 
@@ -19,26 +19,26 @@ define( 'BWS_PLUGIN_VERSION' , '1.0.0' ) ;
 
 register_activation_hook( __FILE__ , 'bws_activate_with_default_options' ) ;
 function bws_activate_with_default_options() {
-  $bws_plugin_options = array(
-    'disable_pages_widget' => 0 , 
-    'disable_search_widget' => 0 , 
-    'disable_categories_widget' => 0 ,
-    'disable_archives_widget' => 0 ,
-  ) ;
-  update_option( 'bws_plugin_options' , $bws_plugin_options ) ;
+	$bws_plugin_options = array(
+	'disable_pages_widget' => 0 ,
+	'disable_search_widget' => 0 ,
+	'disable_categories_widget' => 0 ,
+	'disable_archives_widget' => 0 ,
+	) ;
+	update_option( 'bws_plugin_options' , $bws_plugin_options ) ;
 }
 
 add_action( 'plugins_loaded' , 'bws_load_textdomain' ) ;
 function bws_load_textdomain() {
-  load_plugin_textdomain( 'bootstrap-widget-styling' , false , basename( dirname( __FILE__ ) ) . '/languages' ) ;
+	load_plugin_textdomain( 'bootstrap-widget-styling' , false , basename( dirname( __FILE__ ) ) . '/languages' ) ;
 }
 
 add_action( 'plugins_loaded' , 'bws_get_included_files' ) ;
 function bws_get_included_files() {
-  $included_files = array( 'bws-options' , 'bws-widget-filters' , 'class-bws-settings-fields' ,
-    		    	   'class-bws-settings-page' , 'class-bws-filter' , 'class-bws-search-widget' 
-  ) ; 
-  foreach( $included_files as $file ) {
-    include_once( plugin_dir_path( __FILE__ ) . "includes/{$file}.php" ) ;
-  }
+	$included_files = array( 'bws-options' , 'bws-widget-filters' , 'class-bws-settings-fields' ,
+				'class-bws-settings-page' , 'class-bws-filter' , 'class-bws-search-widget'
+	) ;
+	foreach( $included_files as $file ) {
+	include_once( plugin_dir_path( __FILE__ ) . "includes/{$file}.php" ) ;
+	}
 }
